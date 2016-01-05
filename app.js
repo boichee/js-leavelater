b465449b59d6fca54d4a5a586852fc71bbcd3f45
Brett Levenson <blevenson@evernote.com>
2016-01-04 19:25:16 -0800

    Cleaning things up a bit and making it so that when traffic is greater than desired the program will continue to run.
    Added:
    1. You can now set the desired max proportion of normal as an argument. It will be used to say when to leave.
    2. You can now set the time between checks as an argument (in minutes)
    3. The calculation of trip time (as a percentage) is rounded to two decimal places.
    
    Encapsulated the API call in a function to enable it to call itself via setTimeout.
    
    Default values for both acceptableProportion and nextCheckTime were added

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ var request = require('superagent');
 var program = require('commander');
 var colors = require('colors');
 
+// Helpers when calculating time in milliseconds
+const ONE_SECOND = 1000;
+const ONE_MINUTE = ONE_SECOND * 60;
+
 program
     .version('0.0.1')
     .usage('[options]')
     .option('-o, --origin <address or place>', 'Starting point for traffic calc')
     .option('-d, --destination <address or place>', 'Destination for traffic calc')
+    .option('-p, --acceptable-proportion <proportion>', 'Acceptable proportion to say to leave in.')
+    .option('-i, --check-interval <minutes>', 'How often to check in minutes.')
     .parse(process.argv);
 
 
@@ -17,6 +23,10 @@ if (!program.origin || !program.destination) {
     program.help();
 };
 
+if(program.checkInterval) {
+    var checkInterval = program.checkInterval * ONE_MINUTE;
+}
+
 var params = Object.create({
     key: process.env.GOOGLE_DIRECTIONS_API_TOKEN,
     origin: program.origin,
@@ -24,38 +34,53 @@ var params = Object.create({
     departure_time: 'now'
 });
 
-request
-    .get('https://maps.googleapis.com/maps/api/directions/json')
-    .query(params)
-    .then(function(resp) {
-        if (!resp.body) {
-            console.error('A HTTP error or some other crazy error occurred. That\'s all I know.');
-            process.exit(1);
-        } else if (resp.body.status === 'NOT_FOUND' ) {
-            console.error('The origin or destination you provided could not be reverse geocoded. Try entering a complete address instead.'.red);
-            process.exit(1);
-        } else if (resp.body.error_message) {
-            console.error('Something went wrong while calling the API: %s'.red, resp.body.error_message);
-            process.exit(1);
-        }
+checkTripTime(params, program.acceptableProportion, checkInterval);
 
-        console.log(resp.body);
-        
-        var leg = resp.body.routes[0].legs[0];
+function checkTripTime(params, acceptableProportion, nextCheckTime) {
+    acceptableProportion = acceptableProportion || 1.15; // Default to 115% of normal
+    nextCheckTime = nextCheckTime || ONE_MINUTE * 5; // Default to five minutes
 
-        var closeEnough = leg.duration.value * 1.15; // essentially, we're allowing for 115% of normal.
-        var trafficTime = leg.duration_in_traffic.value;
+    request
+        .get('https://maps.googleapis.com/maps/api/directions/json')
+        .query(params)
+        .then(function(resp) {
+            if (!resp.body) {
+                console.error('A HTTP error or some other crazy error occurred. That\'s all I know.');
+                process.exit(1);
+            } else if (resp.body.status === 'NOT_FOUND' ) {
+                console.error('The origin or destination you provided could not be reverse geocoded. Try entering a complete address instead.'.red);
+                process.exit(1);
+            } else if (resp.body.error_message) {
+                console.error('Something went wrong while calling the API: %s'.red, resp.body.error_message);
+                process.exit(1);
+            }
 
-        var shouldLeave = closeEnough >= trafficTime;
+            // console.log(resp.body);
+            
+            var leg = resp.body.routes[0].legs[0];
 
-        if (!shouldLeave) {
-            console.info('Trip time is currently %d percent of normal.'.red, trafficTime/leg.duration.value * 100);
-            console.info('Monitoring in progress. You\'ll be notified when it\'s time to leave.');
-        } else {
-            console.info('You can now leave. Your trip is expected to take %s.'.green, leg.duration_in_traffic.text);
-        }
+            var normalTime = leg.duration;
+            var trafficTime = leg.duration_in_traffic;
+            var closeEnough = normalTime.value * acceptableProportion;
+            var currentProportionOfNormal = trafficTime.value / normalTime.value;
 
-    }, function(err) {
-        console.error('An error occurred!'.red);
-        program.help();
-    });
+            console.info('Normal trip time is %s', normalTime.text);
+            console.info('Time with traffic is currently %s', trafficTime.text);
+
+            if (closeEnough < trafficTime.value) {
+                console.info('Trip time is currently %d%% of normal.'.red, (currentProportionOfNormal * 100).toFixed(2));
+                console.info('Monitoring in progress... next check in %d %s.', Math.round(nextCheckTime/ONE_MINUTE, 1), Math.round(nextCheckTime/ONE_MINUTE, 1) > 1 ? 'minutes' : 'minute');
+
+                setTimeout(() => {
+                    checkTripTime(params, acceptableProportion, nextCheckTime);
+                }, nextCheckTime); // Check every 5 minutes for the moment
+            } else {
+                console.info('You can now leave. Traffic is %d%% of normal and your trip is expected to take %s.'.green, (currentProportionOfNormal * 100).toFixed(2), trafficTime.text);
+            }
+
+        }, function(err) {
+            console.error('An error occurred!'.red);
+            program.help();
+        });
+}
+
